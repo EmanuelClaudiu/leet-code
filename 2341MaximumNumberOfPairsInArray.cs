8198a5f825e00b106796ddcabbb081bbea7ca020
Emanuel Ignat <EIgnat@boylesports.com>
2026-01-14 19:46:22 +0200

    451 & 2341

---
+++ 2341MaximumNumberOfPairsInArray.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode
+{
+    internal class _2341MaximumNumberOfPairsInArray
+    {
+        private Dictionary<int, int> _occurences = [];
+        private int _duplicates = 0;
+
+        public void Run()
+        {
+            int[] nums = [1, 3, 2, 1, 3, 2, 2];
+
+            var sol = NumberOfPairs(nums);
+
+            foreach (var num in sol)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+        }
+
+        public int[] NumberOfPairs(int[] nums)
+        {
+            var leftover = ProcessOutDuplicates(nums);
+
+            return [_duplicates, leftover.Length];
+        }
+
+        private int[] ProcessOutDuplicates(int[] nums)
+        {
+            _occurences = [];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (_occurences.Keys.Contains(nums[i]))
+                {
+                    _duplicates++;
+                    var duplicateIndex = _occurences[nums[i]];
+                    _occurences.Remove(nums[i]);
+
+                    var arrayUntilDuplicate = SubArray(nums, 0, duplicateIndex);
+                    var arrayFromDuplicateToCurrent = SubArray(nums, duplicateIndex + 1, i - duplicateIndex - 1);
+                    var arrayFromCurrentToEnd = SubArray(nums, i + 1, nums.Length - i - 1);
+
+                    return ProcessOutDuplicates([.. arrayUntilDuplicate, .. arrayFromDuplicateToCurrent, .. arrayFromCurrentToEnd]);
+                }
+
+                _occurences.Add(nums[i], i);
+            }
+
+            return nums;
+        }
+
+        private int[] SubArray(int[] data, int index, int length)
+        {
+            int[] result = new int[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+    }
+}
---
+++ 451SortCharactersByFrequency.cs
@@ -0,0 +1,70 @@
+﻿namespace LeetCode
+{
+    internal class _451SortCharactersByFrequency
+    {
+        private Dictionary<char, int> _frequencies = [];
+        private Dictionary<int, HashSet<char>> _frequencyPerCharacters = [];
+
+        public void Run()
+        {
+            string s = "Aabb";
+
+            var sol = FrequencySort(s);
+
+            Console.WriteLine(sol);
+        }
+
+        public string FrequencySort(string s)
+        {
+            foreach (var character in s)
+            {
+                if (!_frequencies.Keys.Contains(character))
+                {
+                    _frequencies[character] = 1;
+                    if (!_frequencyPerCharacters.Keys.Contains(1)) _frequencyPerCharacters[1] = [];
+                    _frequencyPerCharacters[1].Add(character);
+                    continue;
+                }
+
+                _frequencies[character] += 1;
+                if (!_frequencyPerCharacters.Keys.Contains(_frequencies[character])) _frequencyPerCharacters[_frequencies[character]] = [];
+                _frequencyPerCharacters[_frequencies[character]].Add(character);
+                _frequencyPerCharacters[_frequencies[character] - 1].Remove(character);
+            }
+
+            string frequencyString = "";
+
+            var frequencies = _frequencyPerCharacters.Keys.ToArray();
+
+            for (int i = frequencies.Length - 1; i >= 0; i--)
+            {
+                var charactersSet = _frequencyPerCharacters[frequencies[i]];
+
+                char[] characters = [];
+                foreach (var character in charactersSet)
+                {
+                    characters = [.. characters, character];
+                }
+
+                for (int j = characters.Length - 1; j >= 0; j--)
+                {
+                    frequencyString += CharTimesFrequency(characters[j], frequencies[i]);
+                }
+            }
+
+            return frequencyString;
+        }
+
+        private string CharTimesFrequency(char character, int frequency)
+        {
+            string toReturn = "";
+
+            for (int i = 1; i <= frequency; i++)
+            {
+                toReturn += character;
+            }
+
+            return toReturn;
+        }
+    }
+}
