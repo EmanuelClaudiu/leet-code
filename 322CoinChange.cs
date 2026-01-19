ac082cb8ae8286984123982c708aba16ea3cab60
Emanuel Ignat <EIgnat@boylesports.com>
2026-01-19 18:53:40 +0200

    coin change

---
+++ 322CoinChange.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode
+{
+    internal class _322CoinChange
+    {
+        public void Run()
+        {
+            int[] coins = [1, 2147483647];
+            int amount = 2;
+
+            Console.WriteLine(CoinChange(coins, amount));
+        }
+
+        public int CoinChange(int[] coins, int amount)
+        {
+            int[] memo = new int[amount + 1];
+            Array.Fill(memo, -2);
+
+            return Process(coins, amount, memo);
+        }
+
+        public int Process(int[] coins, int amount, int[] memo)
+        {
+            if (amount == 0) return 0;
+            if (amount < 0) return -1;
+
+            if (memo[amount] != -2) return memo[amount];
+
+            int minCoins = int.MaxValue;
+
+            foreach (int coin in coins)
+            {
+                int result = Process(coins, amount - coin, memo);
+
+                if (result >= 0)
+                {
+                    minCoins = Math.Min(minCoins, result + 1);
+                }
+            }
+
+            memo[amount] = (minCoins == int.MaxValue) ? -1 : minCoins;
+            return memo[amount];
+        }
+    }
+}
