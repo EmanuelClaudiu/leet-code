023fbfe42306bcef839f557c4a539edbe0b3e8f9
Emanuel Ignat <EIgnat@boylesports.com>
2026-01-23 11:51:32 +0200

    75 + 79

---
+++ 75SortColors.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode
+{
+    internal class _75SortColors
+    {
+        private Dictionary<int, int> _colors = new Dictionary<int, int>
+        {
+            { 0, 0 },
+            { 1, 0 },
+            { 2, 0 }
+        };
+
+        public void Run()
+        {
+            int[] nums = [2, 0, 2, 1, 1, 0];
+
+            SortColors(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public void SortColors(int[] nums)
+        {
+            foreach (var color in nums)
+            {
+                _colors[color] += 1;
+            }
+
+            int index = 0;
+
+            for (int i = 1; i <= _colors[0]; i++)
+            {
+                nums[index] = 0;
+                index++;
+            }
+
+            for (int i = 1; i <= _colors[1]; i++)
+            {
+                nums[index] = 1;
+                index++;
+            }
+
+            for (int i = 1; i <= _colors[2]; i++)
+            {
+                nums[index] = 2;
+                index++;
+            }
+        }
+    }
+}
---
+++ 79WordSearch.cs
@@ -0,0 +1,112 @@
+﻿namespace LeetCode
+{
+    internal class _79WordSearch
+    {
+        private Dictionary<char, int[][]> _positions = [];
+
+        public void Run()
+        {
+            //char[][] board = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']];
+            //string word = "ABCCED";
+
+            //char[][] board = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']];
+            //string word = "SEE";
+
+            //char[][] board = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']];
+            //string word = "ABCB";
+
+            char[][] board = [['A']];
+            string word = "AB";
+
+            Console.WriteLine(Exist(board, word));
+        }
+
+        public bool Exist(char[][] board, string word)
+        {
+            int m = board.Length;
+            int n = board[0].Length;
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    if (!_positions.Keys.Contains(board[i][j])) _positions[board[i][j]] = [];
+
+                    _positions[board[i][j]] = [.. _positions[board[i][j]], [i, j]];
+                }
+            }
+
+            if (word.Length == 1)
+            {
+                return _positions.Keys.Contains(word[0]);
+            }
+
+            foreach (var letter in word)
+            {
+                if (!_positions.ContainsKey(letter)) return false;
+            }
+
+            return Process(board, word);
+        }
+
+        private bool Process(char[][] board, string word)
+        {
+            var firstLetter = word[0];
+
+            if (!_positions.Keys.Contains(firstLetter)) return false;
+
+            foreach (var position in _positions[firstLetter])
+            {
+                var visited = new HashSet<string>
+                {
+                    $"{position[0]}-{position[1]}"
+                };
+                if (Compute(board, word[1..], position, visited)) return true;
+            }
+
+            return false;
+        }
+        
+        private bool Compute(char[][] board, string word, int[] currentPosition, HashSet<string> visitedPositions)
+        {
+            var currentLetter = board[currentPosition[0]][currentPosition[1]];
+            var secondLetter = word[0];
+
+            if (!_positions.Keys.Contains(secondLetter)) return false;
+
+            foreach (var position in _positions[secondLetter])
+            {
+                if (visitedPositions.Contains($"{position[0]}-{position[1]}")) continue;
+
+                if (Neighbors(currentPosition, position))
+                {
+                    if (word.Length == 1) return true;
+
+                    var visited = new HashSet<string>(visitedPositions)
+                    {
+                        $"{position[0]}-{position[1]}"
+                    };
+
+                    if (Compute(board, word[1..], position, visited)) return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool Neighbors(int[] positionA, int[] positionB)
+        {
+            if (positionA[0] == positionB[0])
+            {
+                return Math.Abs(positionA[1] - positionB[1]) == 1;
+            }
+
+            if (positionA[1] == positionB[1])
+            {
+                return Math.Abs(positionA[0] - positionB[0]) == 1;
+            }
+
+            return false;
+        }
+    }
+}
