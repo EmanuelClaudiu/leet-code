c6c710ab6613446377926e33ae6efdc0b7e203f8
Emanuel Ignat <EIgnat@boylesports.com>
2026-01-27 11:07:34 +0200

    minesweeper fun

---
+++ 2101DetonateMaximumBombs.cs
@@ -0,0 +1,84 @@
+﻿namespace LeetCode
+{
+    internal class _2101DetonateMaximumBombs
+    {
+        private Dictionary<int, int[]> _vertices = [];
+
+        public void Run()
+        {
+            int[][] bombs = [[2, 1, 3], [6, 1, 4]];
+            //int[][] bombs = [[1, 1, 5], [10, 10, 5]];
+            //int[][] bombs = [[1, 2, 3], [2, 3, 1], [3, 4, 2], [4, 5, 3], [5, 6, 4]];
+
+            var sol = MaximumDetonation(bombs);
+
+            Console.WriteLine(sol);
+        }
+
+        public int MaximumDetonation(int[][] bombs)
+        {
+            for (int i = 0; i < bombs.Length - 1; i++)
+            {
+                for (int j = i + 1; j < bombs.Length; j++)
+                {
+                    if (IsVertex(bombs[i], bombs[j]))
+                    {
+                        if (!_vertices.ContainsKey(i)) _vertices[i] = [];
+                        _vertices[i] = [.. _vertices[i], j];
+                    }
+                    if (IsVertex(bombs[j], bombs[i]))
+                    {
+                        if (!_vertices.ContainsKey(j)) _vertices[j] = [];
+                        _vertices[j] = [.. _vertices[j], i];
+                    }
+                }
+            }
+
+            var maxBombsDetonated = 1;
+
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                var detonated = DetonateFromStart(i);
+                if (detonated > maxBombsDetonated) maxBombsDetonated = detonated;
+            }
+
+            return maxBombsDetonated;
+        }
+
+        private int DetonateFromStart(int root)
+        {
+            var detonateQueue = new Queue<int>();
+            var alreadyDetonated = new HashSet<int>();
+            detonateQueue.Enqueue(root);
+            var detonated = 0;
+
+            while (detonateQueue.Count != 0)
+            {
+                var currentBomb = detonateQueue.Dequeue();
+
+                if (alreadyDetonated.Contains(currentBomb)) continue;
+
+                detonated++;
+                alreadyDetonated.Add(currentBomb);
+
+                if (!_vertices.ContainsKey(currentBomb)) continue;
+
+                foreach (var vertex in _vertices[currentBomb])
+                {
+                    detonateQueue.Enqueue(vertex);
+                }
+            }
+
+            return detonated;
+        }
+
+        private bool IsVertex(int[] node1, int[] node2)
+        {
+            var squaredXDifference = Math.Pow(Math.Abs(node1[0] - node2[0]), 2);
+            var squaredYDifference = Math.Pow(Math.Abs(node1[1] - node2[1]), 2);
+            var squaredRadius = Math.Pow(node1[2], 2);
+
+            return squaredXDifference + squaredYDifference <= squaredRadius;
+        }
+    }
+}
---
+++ 529Minesweeper.cs
@@ -0,0 +1,105 @@
+﻿namespace LeetCode
+{
+    internal class _529Minesweeper
+    {
+        private HashSet<string> _updated = [];
+
+        public void Run()
+        {
+            //char[][] board = [
+            //    ['E', 'E', 'E', 'E', 'E'], 
+            //    ['E', 'E', 'M', 'E', 'E'], 
+            //    ['E', 'E', 'E', 'E', 'E'], 
+            //    ['E', 'E', 'E', 'E', 'E']];
+            //int[] click = [3, 0];
+
+            char[][] board = [
+                ['B', '1', 'E', '1', 'B'],
+                ['B', '1', 'M', '1', 'B'],
+                ['B', '1', '1', '1', 'B'],
+                ['B', 'B', 'B', 'B', 'B']];
+            int[] click = [1, 2];
+
+            var sol = UpdateBoard(board, click);
+
+            foreach (var row in sol)
+            {
+                foreach (var elem in row)
+                {
+                    Console.Write($"{elem} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public char[][] UpdateBoard(char[][] board, int[] click)
+        {
+            if (IsBomb(board, click[0], click[1]))
+            {
+                board[click[0]][click[1]] = 'X';
+                return board;
+            }
+
+            board = Update(board, click);
+
+            return board;
+        }
+
+        private char[][] Update(char[][] board, int[] position)
+        {
+            var x = position[0];
+            var y = position[1];
+            if (_updated.Contains($"{x}-{y}")) return board;
+
+            var rows = board.Length;
+            var cols = board[0].Length;
+            if (x < 0 || x >= rows || y < 0 || y >= cols) return board;
+
+            var adjacentBombs = AdjacentBombs(board, x, y);
+            _updated.Add($"{x}-{y}");
+
+            if (adjacentBombs > 0)
+            {
+                board[x][y] = adjacentBombs.ToString()[0];
+                return board;
+            }
+
+            board[x][y] = 'B';
+
+            board = Update(board, [x - 1, y - 1]);
+            board = Update(board, [x - 1, y]);
+            board = Update(board, [x - 1, y + 1]);
+            board = Update(board, [x, y - 1]);
+            board = Update(board, [x, y + 1]);
+            board = Update(board, [x + 1, y - 1]);
+            board = Update(board, [x + 1, y]);
+            board = Update(board, [x + 1, y + 1]);
+
+            return board;
+        }
+
+        private int AdjacentBombs(char[][] board, int x, int y)
+        {
+            var adjacentBombs = 0;
+
+            if (IsBomb(board, x - 1, y - 1)) adjacentBombs++;
+            if (IsBomb(board, x - 1, y)) adjacentBombs++;
+            if (IsBomb(board, x - 1, y + 1)) adjacentBombs++;
+            if (IsBomb(board, x, y - 1)) adjacentBombs++;
+            if (IsBomb(board, x, y + 1)) adjacentBombs++;
+            if (IsBomb(board, x + 1, y - 1)) adjacentBombs++;
+            if (IsBomb(board, x + 1, y)) adjacentBombs++;
+            if (IsBomb(board, x + 1, y + 1)) adjacentBombs++;
+
+            return adjacentBombs;
+        }
+
+        private bool IsBomb(char[][] board, int x, int y)
+        {
+            if (x < 0 || x >= board.Length) return false;
+            if (y < 0 || y >= board[0].Length) return false;
+
+            return board[x][y] == 'M';
+        }
+    }
+}
